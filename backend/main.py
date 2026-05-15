4ac521e25ae9be5e334b6e3d0855ed7c28c84eb9
u20236072-キサカ <guimuban7@gmail.com>
2026-05-15 15:33:42 +0900

    fix api url

--- backend/main.py
+++ backend/main.py
@@ -20,8 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://oc-web-5gl4.vercel.app",
-        "https://oc-web-noj5.vercel.app"
+        "https://oc-web-oldz.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
